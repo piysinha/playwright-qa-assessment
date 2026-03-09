e74caa8670bf64ab5add8c37bbf57c88d93c5a67
Piyush Sinha <piyush@Piyushs-MacBook-Air.local>
2026-03-09 16:19:43 +0530

    Trigger CI Build using push command

--- tests/api/users.spec.ts
+++ tests/api/users.spec.ts
@@ -11,6 +11,8 @@ test('GET users', async ({ request }) => {
     }
   });
 
+  //Testing environment variable
+
   // console.log(process.env.REQRES_API_KEY);
 
   expect(response.status()).toBe(200);
